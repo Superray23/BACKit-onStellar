9ade85563c808b49467692692ff2fd5491b1e7e0
temycodes <temycodes@gmail.com>
2026-06-04 19:08:07 +0100

    fix: remove unused GlobalExceptionFilter import

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,7 +3,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
